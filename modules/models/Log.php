3302c600bb87f57c97908ead2a61f72063665f6a
Bogdanek <bogdan@aurocraft.com>
2018-04-01 12:55:57 +0300

    update form

---
+++ modules/models/Log.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\modules\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "log".
+ *
+ * @property int $id
+ * @property string $text
+ */
+class Log extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'log';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['text'], 'string'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'text' => 'Text',
+        ];
+    }
+}
