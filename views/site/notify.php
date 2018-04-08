f900e9c7f96dbc3b48a35d3d3d79fc81f697413c
Bogdanek <bogdan@aurocraft.com>
2018-04-08 21:49:38 +0300

    change notify

--- views/site/notify.php
+++ views/site/notify.php
@@ -57,9 +57,25 @@ if (strcmp ($res, "VERIFIED") == 0) {
     // process the notification
     // assign posted variables to local variables
 
-    $model = new \app\modules\models\Log();
-    $model->text = json_encode($_POST);
-    $model->save();
+    $log = new \app\modules\models\Log();
+    $log->text = json_encode($_POST);
+    $log->save();
+
+    if(!empty($_POST)){
+        $model = \app\models\Payment::find()
+            ->where(['payment_order_id' => $_POST['custom']])
+            ->one();
+
+        if ($_POST['payment_status' == 'Completed']){
+            $model->status = 1;
+        } else {
+            $model->status = 2;
+        }
+
+        $model->save();
+    }
+
+
 
 } else if (strcmp ($res, "INVALID") == 0) {
     $model = new \app\modules\models\Log();
