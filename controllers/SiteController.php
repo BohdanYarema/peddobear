f6f0c56004b4c23460dac2db6cbf88cb5a2fc56b
Bogdanek <bogdan@aurocraft.com>
2018-04-08 17:54:26 +0300

    fix notify

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -134,7 +134,7 @@ class SiteController extends Controller
             $count += $item->count;
         }
 
-        $paypalEmail    = "Shop@tedacar.eu";
+        $paypalEmail    = "shop-facilitator@tedacar.eu";
         $paypalURL      = "https://www.paypal.com/cgi-bin/webscr";
         $currency       = Yii::$app->params['delivery'][Yii::$app->language]['currency'];
         $itemName       = "Peddobear purchase";
