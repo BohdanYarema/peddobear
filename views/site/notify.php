0caff02729fa5f19e9e2849a9a5b350e3827fb0e
Bogdanek <bogdan@aurocraft.com>
2018-04-08 22:26:38 +0300

    fixed meyby final

--- views/site/notify.php
+++ views/site/notify.php
@@ -47,36 +47,43 @@ curl_close($ch);
 ?>
 
 <?php
+// inspect IPN validation result and act accordingly
+if (strcmp ($res, "VERIFIED") == 0) {
+    // The IPN is verified, process it:
+    // check whether the payment_status is Completed
+    // check that txn_id has not been previously processed
+    // check that receiver_email is your Primary PayPal email
+    // check that payment_amount/payment_currency are correct
+    // process the notification
+    // assign posted variables to local variables
 
-$model = new \app\modules\models\Log();
-$model->text = $res;
-$model->save();
+    $log = new \app\modules\models\Log();
+    $log->text = json_encode($_POST);
+    $log->save();
 
-//// inspect IPN validation result and act accordingly
-//if (strcmp ($res, "VERIFIED") == 0) {
-//    // The IPN is verified, process it:
-//    // check whether the payment_status is Completed
-//    // check that txn_id has not been previously processed
-//    // check that receiver_email is your Primary PayPal email
-//    // check that payment_amount/payment_currency are correct
-//    // process the notification
-//    // assign posted variables to local variables
-//
-////    $log = new \app\modules\models\Log();
-////    $log->text = json_encode($_POST);
-////    $log->save();
-//
-//    $model = \app\models\Payment::find()->all();
-//    foreach ($model as $item) {
-//        $item->status = 1;
-//        $item->save();
-//    }
-//
-//
-//
-//} else if (strcmp ($res, "INVALID") == 0) {
-//    $model = new \app\modules\models\Log();
-//    $model->text = json_encode($_POST);
-//    $model->save();
-//}
+    if(!empty($_POST)){
+        $model = \app\models\Payment::find()
+            ->where(['payment_order_id' => $_POST['custom']])
+            ->one();
+        if ($_POST['payment_status'] == 'Completed'){
+            $model->status = 1;
+        } else {
+            $model->status = 2;
+        }
+        $model->save();
+    }
+
+
+
+} else if (strcmp ($res, "INVALID") == 0) {
+    $log = new \app\modules\models\Log();
+    $log->text = json_encode($_POST);
+    $log->save();
+
+    $model = \app\models\Payment::find()
+        ->where(['payment_order_id' => $_POST['custom']])
+        ->one();
+    $model->status = 3;
+    $model->save();
+}
 ?>
