de2111458089cf928c13f2080b80b626f6c8fe2f
Bogdanek <bogdan@aurocraft.com>
2018-04-14 09:43:24 +0300

    fix notify

--- views/site/notify.php
+++ views/site/notify.php
@@ -6,6 +6,9 @@
  * Time: 08:58
  */
 
+$raw_post_data = file_get_contents('php://input');
+$raw_post_array = explode('&', $raw_post_data);
+
 $log = new \app\modules\models\Log();
-$log->text = json_encode($_POST);
+$log->text = json_encode($raw_post_array);
 $log->save();
