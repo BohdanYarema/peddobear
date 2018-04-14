e641f7dc4e70462b996de347c92f3b55b04c40e7
Bogdanek <bogdan@aurocraft.com>
2018-04-14 08:58:52 +0300

    tes notify

--- views/site/notify.php
+++ views/site/notify.php
@@ -1,89 +1,11 @@
 <?php
-// STEP 1: read POST data
-// Reading POSTed data directly from $_POST causes serialization issues with array data in the POST.
-// Instead, read raw POST data from the input stream.
-$raw_post_data = file_get_contents('php://input');
-$raw_post_array = explode('&', $raw_post_data);
-$myPost = array();
-foreach ($raw_post_array as $keyval) {
-    $keyval = explode ('=', $keyval);
-    if (count($keyval) == 2)
-        $myPost[$keyval[0]] = urldecode($keyval[1]);
-}
-// read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
-$req = 'cmd=_notify-validate';
-if (function_exists('get_magic_quotes_gpc')) {
-    $get_magic_quotes_exists = true;
-}
-foreach ($myPost as $key => $value) {
-    if ($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
-        $value = urlencode(stripslashes($value));
-    } else {
-        $value = urlencode($value);
-    }
-    $req .= "&$key=$value";
-}
+/**
+ * Created by PhpStorm.
+ * User: Bogdanek
+ * Date: 14.04.18
+ * Time: 08:58
+ */
 
-// Step 2: POST IPN data back to PayPal to validate
-$ch = curl_init('https://ipnpb.paypal.com/cgi-bin/webscr');
-curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
-// In wamp-like environments that do not come bundled with root authority certificates,
-// please download 'cacert.pem' from "https://curl.haxx.se/docs/caextract.html" and set
-// the directory path of the certificate as shown below:
-// curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
-if ( !($res = curl_exec($ch)) ) {
-    // error_log("Got " . curl_error($ch) . " when processing IPN data");
-    curl_close($ch);
-    exit;
-}
-curl_close($ch);
-?>
-
-<?php
-// inspect IPN validation result and act accordingly
-if (strcmp ($res, "VERIFIED") == 0) {
-    // The IPN is verified, process it:
-    // check whether the payment_status is Completed
-    // check that txn_id has not been previously processed
-    // check that receiver_email is your Primary PayPal email
-    // check that payment_amount/payment_currency are correct
-    // process the notification
-    // assign posted variables to local variables
-
-    $log = new \app\modules\models\Log();
-    $log->text = json_encode($_POST);
-    $log->save();
-
-    if(!empty($_POST)){
-        $model = \app\models\Payment::find()
-            ->where(['payment_order_id' => $_POST['custom']])
-            ->one();
-        if ($_POST['payment_status'] == 'Completed'){
-            $model->status = 1;
-        } else {
-            $model->status = 2;
-        }
-        $model->save();
-    }
-
-
-
-} else if (strcmp ($res, "INVALID") == 0) {
-    $log = new \app\modules\models\Log();
-    $log->text = json_encode($_POST);
-    $log->save();
-
-    $model = \app\models\Payment::find()
-        ->where(['payment_order_id' => $_POST['custom']])
-        ->one();
-    $model->status = 3;
-    $model->save();
-}
-?>
+$log = new \app\modules\models\Log();
+$log->text = json_encode($_POST);
+$log->save();
--- views/site/test.php
+++ views/site/test.php
@@ -1,88 +1,47 @@
 <?php
-//$ch = curl_init();
-//
-//curl_setopt($ch, CURLOPT_URL, "https://secure.payu.com/api/v2_1/orders/");
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-//curl_setopt($ch, CURLOPT_HEADER, FALSE);
-//
-//curl_setopt($ch, CURLOPT_POST, TRUE);
-//
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-//  \"notifyUrl\": \"https://peddobear.local.test/test\",
-//  \"customerIp\": \"127.0.0.1\",
-//  \"merchantPosId\": \"586502\",
-//  \"description\": \"RTV market\",
-//  \"currencyCode\": \"PLN\",
-//  \"totalAmount\": \"100\",
-//  \"products\": [
-//    {
-//      \"name\": \"Wireless mouse\",
-//      \"unitPrice\": \"15000\",
-//      \"quantity\": \"1\"
-//    },
-//    {
-//      \"name\": \"HDMI cable\",
-//      \"unitPrice\": \"6000\",
-//      \"quantity\": \"1\"
-//    }
-//  ]
-//}");
-//
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//    "Content-Type: application/json",
-//    "Authorization: Bearer 57b418fb-da2d-49f7-bb4e-521a2b17e6d1"
-//));
-//
-//$response = curl_exec($ch);
-//curl_close($ch);
-//
-//var_dump($response);
+$ch = curl_init();
 
+curl_setopt($ch, CURLOPT_URL, "https://secure.payu.com/api/v2_1/orders/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
-////set Production Environment
-//OpenPayU_Configuration::setEnvironment('secure');
-//
-////set POS ID and Second MD5 Key (from merchant admin panel)
-//OpenPayU_Configuration::setMerchantPosId('586502');
-//OpenPayU_Configuration::setSignatureKey('c5f55fbd51ca1d45409a2b4c36e6231f');
-//
-////set Oauth Client Id and Oauth Client Secret (from merchant admin panel)
-//OpenPayU_Configuration::setOauthClientId('586502');
-//OpenPayU_Configuration::setOauthClientSecret('c2d8d791dc3f043115820e22261e6dda');
-//
-//
-//$order['continueUrl'] = 'http://localhost/'; //customer will be redirected to this page after successfull payment
-//$order['notifyUrl'] = 'http://localhost/';
-//$order['customerIp'] = $_SERVER['REMOTE_ADDR'];
-//$order['merchantPosId'] = OpenPayU_Configuration::getMerchantPosId();
-//$order['description'] = 'New order';
-//$order['currencyCode'] = 'PLN';
-//$order['totalAmount'] = 3200;
-//$order['extOrderId'] = '1342111111'; //must be unique!
-//
-//$order['products'][0]['name'] = 'Product1';
-//$order['products'][0]['unitPrice'] = 1000;
-//$order['products'][0]['quantity'] = 1;
-//
-//$order['products'][1]['name'] = 'Product2';
-//$order['products'][1]['unitPrice'] = 2200;
-//$order['products'][1]['quantity'] = 1;
-//
-////optional section buyer
-//$order['buyer']['email'] = 'dd@ddd.pl';
-//$order['buyer']['phone'] = '123123123';
-//$order['buyer']['firstName'] = 'Jan';
-//$order['buyer']['lastName'] = 'Kowalski';
-//
-//$response = OpenPayU_Order::create($order);
-//
-//header('Location:'.$response->getResponse()->redirectUri);
-//
-//exit();
+curl_setopt($ch, CURLOPT_POST, TRUE);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, "{
+  \"notifyUrl\": \"https://peddobear.local.test/test\",
+  \"customerIp\": \"127.0.0.1\",
+  \"merchantPosId\": \"586502\",
+  \"description\": \"RTV market\",
+  \"currencyCode\": \"PLN\",
+  \"totalAmount\": \"100\",
+  \"products\": [
+    {
+      \"name\": \"Wireless mouse\",
+      \"unitPrice\": \"15000\",
+      \"quantity\": \"1\"
+    },
+    {
+      \"name\": \"HDMI cable\",
+      \"unitPrice\": \"6000\",
+      \"quantity\": \"1\"
+    }
+  ]
+}");
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+    "Authorization: Bearer 2d59a1d1-929d-4a9f-b3cd-a87532d528d1"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+var_dump($response);
 
 
 
-$model = \app\modules\models\Log::find()->all();
-foreach ($model as $item) {
-    print_r(json_decode($item->text, true));
-}
+
+//$model = \app\modules\models\Log::find()->all();
+//foreach ($model as $item) {
+//    print_r(json_decode($item->text, true));
+//}
