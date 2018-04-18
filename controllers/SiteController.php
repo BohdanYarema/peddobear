5ad5d8e3be4b3e6bb57e09ff39e14b9ba37d5495
Bogdan Yarema <b.yarema@mobimill.com>
2018-04-18 11:47:56 +0300

    fix actions

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -370,7 +370,7 @@ class SiteController extends Controller
 
         foreach ($model->paymentItems as $item) {
             $items[]    = [
-                "name"      => htmlentities($item->shop->locale->name),
+                "name"      => htmlentities($item->shop->locale->title),
                 "unitPrice" => $item->summary,
                 "quantity"  => $item->count
             ];
