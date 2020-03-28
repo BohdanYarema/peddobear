0abe9370f09a4754a5d80ce0c60c246be75a78f3
bohdanyarema <bohdanyarema1992@gmail.com>
2020-03-28 19:52:11 +0200

    add interface for payments

--- models/PayPal.php
+++ models/PayPal.php
@@ -16,7 +16,7 @@ class PayPal implements PaymentSystem
     public $returnUrl = 'http://yarema.fun/success';
     public $cancelUrl = 'http://yarema.fun/cancel';
     public $notifyUrl = 'http://yarema.fun/notifypaypal';
-    public $paypalEmail = 'bohdanyarema1992-buyer@gmail.com';
+    public $paypalEmail = 'bohdanyarema1992-facilitator@gmail.com';
     public $paypalURL = 'https://www.sandbox.paypal.com/cgi-bin/webscr';
     public $itemName = 'Ted a Car purchase';
 
@@ -33,7 +33,7 @@ class PayPal implements PaymentSystem
         $querystring = "?business=" . urlencode($this->paypalEmail) . "&";
         $querystring .= "currency_code=" . urlencode($currency) . "&";
         $querystring .= "cmd=" . urlencode('_xclick') . "&";
-        $querystring .= "item_name=" . urlencode($this->itemName) . "&";
+        $querystring .= "item_name=" . urlencode($this->itemName . 'payment id - '. $this->model->payment_order_id) . "&";
         $querystring .= "amount=". urlencode($price) . "&";
         $querystring .= "custom=". urlencode($this->model->payment_order_id) . "&";
 
